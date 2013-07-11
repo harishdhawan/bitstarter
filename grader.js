d63cea7cbbe455e4da02c771441b637d754bb2cd
Harish Dhawan <harish.dhawan@gmail.com>
2013-07-11 06:18:31 +0000

    added url support

--- grader.js
+++ grader.js
@@ -24,7 +24,9 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
+var restler = require('restler');
+var HTMLFILE_DEFAULT = "";
+var URL_DEFAULT = "";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -33,6 +35,7 @@ var assertFileExists = function(infile) {
         console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
+    console.log(">>>>" + instr);
     return instr;
 };
 
@@ -65,10 +68,30 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url to html_file>', 'URL to index.html', clone(function(url){return url;}), URL_DEFAULT)
         .parse(process.argv);
+
+if(program.url != ""){
+
+	console.log("the URL is" + program.url);
+    restler.get(program.url).on('complete', function(data, response) {
+	    console.log(response.headers);
+            fs.writeFileSync( "./tmp.html", data);
+	  console.log();
+	  file = "tmp.html";
+	  var checkJson = checkHtmlFile(file, program.checks);
+	  var outJson = JSON.stringify(checkJson, null, 4);
+	  console.log(outJson);
+	  //replServer.context.data = data;
+    });
+}
+else{
+	console.log("the program file is" + program.file);
+file = program.file;
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+}
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
